8060ebe75bb36da7b6a8c3a67895c8e17d6fcacf
Corbin Davenport <corbindavenport@outlook.com>
2026-01-12 00:48:58 -0500

    Update preview image and site table/header styles

--- .eleventy.js
+++ .eleventy.js
@@ -7,9 +7,18 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("favicon.ico");
     // Add media folder to site
     eleventyConfig.addPassthroughCopy("media");
-    // Set target blank attributes for all external links
-    eleventyConfig.addTransform("update-links", async function (content) {
+    // Modify page contents
+    eleventyConfig.addTransform("update-html", async function (content) {
         const dom = new JSDOM(content);
+        // Update table design
+        dom.window.document.querySelectorAll('table').forEach(function (el) {
+            el.classList.add('table');
+        });
+        // Fix header spacing in cards 
+        dom.window.document.querySelectorAll('.card-body h2, .card-body h3, .card-body h4, .card-body h5, .card-body h6').forEach(function (el) {
+            el.classList.add('mb-3');
+        });
+        // Set target blank attributes for all external links
         dom.window.document.querySelectorAll('a').forEach(function (el) {
             if (el.href.startsWith('https://') || el.href.startsWith('http://')) {
                 el.setAttribute('target', '_blank');
